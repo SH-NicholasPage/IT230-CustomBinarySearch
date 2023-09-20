1a49327553f5d0b7cfd69d9e6eb4f75db92a6ad6
Nicholas Page <npage@southhills.edu>
2023-09-19 20:14:32 -0400

    Initial commit

---
+++ CustomBinarySearch/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Name: [YOUR NAME HERE]
+ * South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+ */
+
+using System;
+using System.Linq;
+
+namespace CustomBinarySearch
+{
+	public class Program
+	{
+		private const uint ARRAY_SIZE = 16;
+
+		private static int BinarySearch(int[] arr, int match)
+		{
+			//TODO
+			return -1;
+		}
+
+		//DO NOT MODIFY THE MAIN METHOD
+		public static void Main()
+		{
+			Random rand = new Random();
+			int[] randNums = new int[ARRAY_SIZE];
+
+			for (int i = 0; i < randNums.Length; i++)
+			{
+				int randNum = rand.Next(0, 100);
+
+				while (randNums.Contains(randNum))
+				{
+					randNum = rand.Next(0, 100);
+				}
+
+				randNums[i] = randNum;
+			}
+
+			Array.Sort(randNums);
+
+			Console.WriteLine($"Numbers generated {String.Join(", ", randNums)}");
+			Console.Write("Enter a number to search for: ");
+			int searchNum = int.Parse(Console.ReadLine()!);
+
+			int index = BinarySearch(randNums, searchNum);
+
+			if(index != -1)
+			{
+				Console.WriteLine($"Number found at index {index}");
+			}
+			else
+			{
+				Console.WriteLine("Number not found");
+			}
+		}
+	}
+}
